89fab031b75cddf4fe51290544c044a7ff439177
Rodrigo Borges <rodrigoborges.si@gmail.com>
2023-10-25 19:03:02 -0300

    fix/adicionando dependencias

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
